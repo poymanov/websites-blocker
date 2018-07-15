65be7448a98d7598e269f010ae251e8850652b18
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-15 17:59:56 +0300

    Add changing hosts file when user has fun time

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from datetime import datetime as dt
 hosts_file = '/etc/hosts'
 redirect_url = '127.0.0.1'
 block_sites = ['facebook.com', 'www.facebook.com']
-begin_period = dt(dt.now().year, dt.now().month, dt.now().day, 17)
-end_period = dt(dt.now().year, dt.now().month, dt.now().day, 18)
+begin_period = dt(dt.now().year, dt.now().month, dt.now().day, 18)
+end_period = dt(dt.now().year, dt.now().month, dt.now().day, 19)
 
 while True:
 	if begin_period < dt.now() < end_period:
@@ -22,4 +22,14 @@ while True:
 
 	else:
 		print('Fun time...')
+
+		with open(hosts_file, 'r+') as file:
+			content = file.readlines()
+			file.seek(0)
+
+			for line in content:
+				if not any(website in line for website in block_sites):
+					file.write(line)
+
+			file.truncate()					
 	time.sleep(5)
