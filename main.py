397176dbd359826c0e109ee217b3f576e5afcf5b
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-15 17:14:11 +0300

    Add working periods params

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import time
+from datetime import datetime as dt
 
 hosts_file = '/etc/hosts'
 redirect_url = '127.0.0.1'
 block_sites = ['facebook.com', 'www.facebook.com']
+begin_period = dt(dt.now().year, dt.now().month, dt.now().day, 18)
+end_period = dt(dt.now().year, dt.now().month, dt.now().day, 19)
 
 while True:
-	print(1)
+	if begin_period < dt.now() < end_period:
+		print('Working time...')
+	else:
+		print('Fun time...')
 	time.sleep(5)
